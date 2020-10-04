fc69836e6b2e4ff32d93e67f470fcb84b7914908
Mohamed Timor <mohamedtimorr@gmail.com>
2020-10-04 21:45:36 +0200

    CRUD Example

---
+++ src/main/java/test/controller/ProductController.java
@@ -0,0 +1,52 @@
+package test.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import test.entity.Product;
+import test.service.ProductService;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+	
+	@PostMapping("/addProduct")
+	public Product addProduct(@RequestBody Product product) {
+		return service.saveProduct(product);
+	}
+	@PostMapping("/addProducts")
+	public List<Product> addProduct(@RequestBody List<Product> products) {
+		return service.saveProducts(products);
+	}
+	@GetMapping("/products")
+	public List<Product> findAllProducts(){
+		return service.getProducts();
+	}
+	@GetMapping("/product/{id}")
+	public Product findProductById(@PathVariable int id) {
+		return service.getProductById(id);
+	}
+	@GetMapping("/product/{name}")
+	public Product findProductByname(@PathVariable String name) {
+		return service.getProductByName(name);
+	}
+	@PutMapping("/update")
+	public Product updateProduct(@RequestBody Product product) {
+		return service.updateProduct(product);
+	}
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable int id) {
+		return service.deleteProduct(id);
+	}
+	
+}
---
+++ src/main/java/test/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import test.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findByName(String name);
+
+	
+}
